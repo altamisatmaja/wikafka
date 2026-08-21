7e7bce0d26fed2badaf7bbd0e95d5184744cf98d
altamisatmaja <altamisfattah16@gmail.com>
2026-08-21 19:34:53 +0700

    feat: initialize

---
+++ src/main/java/bss/atmaja/dev/wikafka/WikafkaApplication.java
@@ -0,0 +1,13 @@
+package bss.atmaja.dev.wikafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bss/atmaja/dev/wikafka/WikafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package bss.atmaja.dev.wikafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
