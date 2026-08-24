dd814c698ab7d74de6a8be665dd329fffa647003
altamisatmaja <altamisfattah16@gmail.com>
2026-08-24 21:24:41 +0700

    feat: add topic

---
+++ kafka-producer-wikafka/src/main/java/bss/atmaja/springboot/KafaTopicConfig.java
@@ -0,0 +1,16 @@
+package bss.atmaja.springboot;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafaTopicConfig {
+
+    @Bean
+    public NewTopic topic(){
+        return TopicBuilder.name("wikimedia_recentchange")
+                .build();
+    }
+}
