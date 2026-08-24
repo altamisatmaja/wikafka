580282a4181abae808e31edc0660857af4de98cb
altamisatmaja <altamisfattah16@gmail.com>
2026-08-24 21:55:06 +0700

    implement and start kafka stream data

--- kafka-producer-wikafka/src/main/java/bss/atmaja/springboot/WikimediaChangesProducer.java
+++ kafka-producer-wikafka/src/main/java/bss/atmaja/springboot/WikimediaChangesProducer.java
@@ -1,24 +1,39 @@
 package bss.atmaja.springboot;
 
+import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.background.BackgroundEventHandler;
+import com.launchdarkly.eventsource.background.BackgroundEventSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.net.URI;
+//import java.util.concurrent.TimeUnit;
 
 @Service
 public class WikimediaChangesProducer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WikimediaChangesProducer.class);
 
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
     public WikimediaChangesProducer(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    private KafkaTemplate<String, String> kafkaTemplate;
+    public void sendMessage() throws InterruptedException {
+        String topic = "wikimedia_recentchange";
 
-    private void sendMessage() {
-        string topic = "wikimedia_recentchange";
+        BackgroundEventHandler eventHandler = new WikimediaChangesHandler(kafkaTemplate, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
 
-    // To realtime stream data from wikimedia, we use event source
+        EventSource.Builder eventSourceBuilder = new EventSource.Builder(URI.create(url));
+
+        try (BackgroundEventSource eventSource = new BackgroundEventSource.Builder(eventHandler, eventSourceBuilder).build()) {
+
+            eventSource.start();
+
+        }
     }
 }
